41789b6b0120107c53ef4d2a4ba4bd4f660dd4c3
Eduardo Silva Rocha <syaoranea@gmail.com>
2024-01-25 22:26:16 -0300

    desabilitando rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'gallery', pathMatch: 'full' },
   {path: 'gallery', loadChildren: () => import('./views/pages/gallery/gallery.module').then(m => m.GalleryModule)},
   {path: 'contact', loadChildren: () => import('./views/pages/contact/contact.module').then(m => m.ContactModule)},
-  {path: 'album', loadChildren: () => import('./views/pages/album/album.module').then(m => m.AlbumModule)},
+  /* {path: 'album', loadChildren: () => import('./views/pages/album/album.module').then(m => m.AlbumModule)}, */
   /* {path: 'blog', loadChildren: () => import('./views/pages/blog/blog.module').then(m => m.BlogModule) *//* ,  canActivate: [AuthGuard] */ /* }, */
   /* {path: 'eventos', loadChildren: () => import('./views/pages/eventos/eventos.module').then(m => m.EventosModule)}, */
   {path: 'obrigado', loadChildren: () => import('./views/pages/thanks/thanks.module').then(m => m.ThanksModule)},
